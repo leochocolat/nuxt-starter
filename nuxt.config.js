c8d419992c30dd5a4b8ceaf8fd73a0078b16904d
leochocolat <l.mouraire@outlook.fr>
2020-05-20 19:44:06 +0200

    add pwa manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,10 +69,22 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxtjs/pwa',
     ['@nuxtjs/google-analytics', {
       id: 'UA-165917651-1'
     }]
   ],
+  /*
+  ** PWA
+  */
+  pwa: {
+    manifest: {
+      name: 'Léo Mouraire | Folio 2020',
+      short_name: "Léo Mouraire",
+      description: "I'm front end developer based in Paris. Currently studying at Gobelins Paris and working at Purée Maison. Looking for a new traineeship for my next degree at Gobelins!",
+      lang: 'en',
+    }
+  },
   /*
   ** Nuxt.js modules
   */

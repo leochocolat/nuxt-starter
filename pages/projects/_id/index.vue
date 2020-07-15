e13c5c3bddb4647b577ef015bc963aa8414b8575
leochocolat <l.mouraire@outlook.fr>
2020-07-15 21:53:54 +0200

    timeout on split text paragraph

--- pages/projects/_id/index.vue
+++ pages/projects/_id/index.vue
@@ -58,8 +58,10 @@ export default {
     },
     methods: {
         setup() {
-            this.setupScrollParagraph();
-
+            setTimeout(() => {
+                this.setupScrollParagraph();
+            }, 200);
+            
             setTimeout(() => {
                 this.state = 'isInView';
             }, 100);

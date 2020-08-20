294e488cce284578ecf1f1c9610a45fee4fe63f0
leochocolat <l.mouraire@outlook.fr>
2020-08-20 20:29:33 +0200

    keep empty folders

---
+++ assets/javascript/components/SampleComponent.js
@@ -0,0 +1,13 @@
+class SampleComponent {
+    constructor(options) {
+        this.el = options.el;
+
+        this._setup();
+    }
+
+    _setup() {
+        
+    }
+}
+
+export default SampleComponent;
